835248f83177a6610036950aa8eabfd622cb20cf
Alex <alex.gyumushyan@gmail.com>
2024-04-29 12:06:32 -0400

    added a basic UI for python to latex conversion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+    <div class="left-box">
+        <h2>Python Code</h2>
+        <textarea bind:value={leftText} placeholder="Enter text here"></textarea>
+    </div>
+    
+    <button on:click={convertText}>Convert</button>
+  
+    <div class="right-box">
+        <h2>Latex</h2>
+        <textarea bind:value={rightText} placeholder="Enter text here"></textarea>
+    </div>
+</div>
+
+
+<script>
+    let leftText = '';
+    let rightText = '';
+
+    function convertText() {
+        console.log('Converted text:', leftText);
+        rightText = leftText
+    }
+</script>
+
+
+<style>
+    .container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  
+    .left-box,
+    .right-box {
+      width: 45%;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+  
+    input {
+      width: 100%;
+      padding: 10px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+  
+    textarea {
+      width: 100%;
+      height: 200px;
+      padding: 10px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+</style>
