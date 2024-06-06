eb69a731dfe63a2464f445d5a7db4e170fd6942b
Alex <alex.gyumushyan@gmail.com>
2024-06-06 09:24:35 -0400

    streamlined lexer

--- src/lib/lexer.ts
+++ src/lib/lexer.ts
@@ -74,93 +74,104 @@ export class Lexer {
                     id += this.currentChar;
                     this.advance();
                 }
-                if (id === 'if') {
-                    return new Token('IF', id);
-                } else if (id === 'elif') {
-                    return new Token('ELIF', id);
-                } else if (id === 'else') {
-                    return new Token('ELSE', id);
-                } else if (id === 'while'){
-                    return new Token('WHILE', id);
-                } else if (id === 'def') {
-                    return new Token('DEF', id);
-                } else if (id === 'return') {
-                    return new Token('RETURN', id);
-                } else if (id === 'for') {
-                    return new Token('FOR', id);
-                } else if (id === 'in') {
-                    return new Token('IN', id);
-                } else {
-                    return new Token('ID', id);
+
+                switch(id){
+                    case 'if':{
+                        return new Token('IF', id);
+                        break;
+                    }
+                    case 'elif':{
+                        return new Token('ELIF', id);
+                        break;
+                    }
+                    case 'else':{
+                        return new Token('ELSE', id);
+                        break;
+                    }
+                    case 'while':{
+                        return new Token('WHILE', id);
+                        break;
+                    }
+                    case 'def':{
+                        return new Token('DEF', id);
+                        break;
+                    }
+                    case 'return':{
+                        return new Token('RETURN', id);
+                        break;
+                    }
+                    case 'for':{
+                        return new Token('FOR', id);
+                        break;
+                    }
+                    case 'in':{
+                        return new Token('IN', id);
+                        break;
+                    }
+                    default:{
+                        return new Token('ID', id);
+                        break;
+                    }
                 }
             }
 
-            if (this.currentChar === '+') {
-                this.advance();
-                return new Token('PLUS', '+');
+            switch(this.currentChar){
+                case '+': {
+                    this.advance();
+                    return new Token('PLUS', '+');    
+                }
+                case '-': {
+                    this.advance();
+                    return new Token('MINUS', '-');    
+                }
+                case '*': {
+                    this.advance();
+                    return new Token('MULTIPLY', '*');    
+                }
+                case '/': {
+                    this.advance();
+                    return new Token('DIVIDE', '/');    
+                }
+                case '(': {
+                    this.advance();
+                    return new Token('LPAREN', '(');    
+                }
+                case ')': {
+                    this.advance();
+                    return new Token('RPAREN', ')');    
+                }
+                case '=': {
+                    this.advance();
+                    return new Token('ASSIGN', '=');    
+                }
+                case '>': {
+                    this.advance();
+                    return new Token('GREATERTHAN', '>');    
+                }
+                case '<': {
+                    this.advance();
+                    return new Token('LESSTHAN', '<');    
+                }
+                case ']': {
+                    this.advance();
+                    return new Token('RIGHTBRACKET', ']');    
+                }
+                case '[': {
+                    this.advance();
+                    return new Token('LEFTBRACKET', '[');    
+                }
+                case ':': {
+                    this.advance();
+                    return new Token('COLON', ':');    
+                }
+                case ',': {
+                    this.advance();
+                    return new Token('COMMA', ',');    
+                }
+                default:{
+                    throw new Error('Invalid character');
+                }
             }
-
-            if (this.currentChar === '-') {
-                this.advance();
-                return new Token('MINUS', '-');
-            }
-
-            if (this.currentChar === '*') {
-                this.advance();
-                return new Token('MULTIPLY', '*');
-            }
-
-            if (this.currentChar === '/') {
-                this.advance();
-                return new Token('DIVIDE', '/');
-            }
-
-            if (this.currentChar === '(') {
-                this.advance();
-                return new Token('LPAREN', '(');
-            }
-
-            if (this.currentChar === ')') {
-                this.advance();
-                return new Token('RPAREN', ')');
-            }
-
-            if (this.currentChar === '=') {
-                this.advance();
-                return new Token('ASSIGN', '=');
-            }
-
-            if (this.currentChar === '>') {
-                this.advance();
-                return new Token('GREATERTHAN', '>');
-            }
-
-            if (this.currentChar === '<') {
-                this.advance();
-                return new Token('LESSTHAN', '<');
-            }
-
-            if (this.currentChar === ':') {
-                this.advance();
-                return new Token('COLON', ':')
-            }
-            
-            if (this.currentChar === ',') {
-                this.advance();
-                return new Token('COMMA', ',')
-            }
-            
-            if (this.currentChar === '[') {
-                this.advance();
-                return new Token('LEFTBRACKET', '[')
-            }
-            
-            if (this.currentChar === ']') {
-                this.advance();
-                return new Token('RIGHTBRACKET', ']')
-            }
-
-            throw new Error('Invalid character');
         }
 
         return new Token('EOF', '');
